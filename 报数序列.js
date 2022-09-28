70088cb7023e05cb39957817c7976ad0a6f68a1a
mashen <undefined>
2022-09-28 22:45:57 +0800

    feat: lint fix

--- 报数序列.js
+++ 报数序列.js
@@ -1,5 +1,5 @@
 const countAndSay = (n) => {
-    let resultStr = '1'
+    let resultStr = '1';
     for (let i = 1; i < n; i++) {
         let repeatCount = 1;
         let str = '';
@@ -7,7 +7,7 @@ const countAndSay = (n) => {
             if (resultStr[j] === resultStr[j + 1]) {
                 repeatCount++;
             } else {
-                str+=repeatCount+resultStr[j];
+                str += repeatCount + resultStr[j];
                 repeatCount = 1;
             }
         }
