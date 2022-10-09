17756dccbe86bc6e07f643eef5d7191d3d1ca95b
mashen <undefined>
2022-10-09 14:15:01 +0800

    feat: 函数柯里化

---
+++ Js实现/函数柯里化.js
@@ -0,0 +1,53 @@
+/**
+ * 柯里化函数
+ */
+// 木易杨
+function currying(fn, length) {
+    length = length || fn.length; // 注释 1
+    return function (...args) { // 注释 2
+        return args.length >= length // 注释 3
+            ?
+            fn.apply(this, args) // 注释 4
+            :
+            currying(fn.bind(this, ...args), ...args) // 注释 5
+    }
+}
+
+// Test
+const fn = currying(function (a, b, c) {
+    console.log([a, b, c]);
+});
+
+fn("a", "b", "c") // ["a", "b", "c"]
+fn("a", "b")("c") // ["a", "b", "c"]
+fn("a")("b")("c") // ["a", "b", "c"]
+fn("a")("b", "c") // ["a", "b", "c"]
+
+
+
+
+/**
+ * 延迟计算
+ */
+
+// 木易杨
+const add = (...args) => args.reduce((a, b) => a + b);
+
+// 简化写法
+function currying(func) {
+    const args = [];
+    return function result(...rest) {
+        if (rest.length === 0) {
+            return func(...args);
+        } else {
+            args.push(...rest);
+            return result;
+        }
+    }
+}
+
+const sum = currying(add);
+
+sum(1, 2)(3); // 未真正求值
+sum(4); // 未真正求值
+sum(); // 输出 10
