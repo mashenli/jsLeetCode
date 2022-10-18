4d7a300f993379b70f339305ce8cf71d95fced12
mashen <undefined>
2022-10-18 15:19:09 +0800

    feat: 判断对象是否有环

---
+++ Js实现/判断对象是否有环引用.js
@@ -0,0 +1,36 @@
+// var obj = {
+//     a: {
+//         c: [
+//             1, 2
+//         ]
+//     },
+//     b: 1
+// }
+// obj.a.c.d = obj
+// console.log(cycleDetector(obj)) // true
+
+// 实现思路：用一个数组存储每一个遍历过的对象，下次找到数组中存在，则说明环引用
+
+function cycleDetector(obj) {
+    const arr = [obj]
+    let flag = false
+
+    function cycle(o) {
+        const keys = Object.keys(o)
+        for (const key of keys) {
+            const temp = o[key]
+            if (typeof temp === 'object' && temp !== null) {
+                if (arr.indexOf(temp) >= 0) {
+                    flag = true
+                    return
+                }
+                arr.push(temp)
+                cycle(temp)
+            }
+        }
+    }
+
+    cycle(obj)
+
+    return flag
+}
