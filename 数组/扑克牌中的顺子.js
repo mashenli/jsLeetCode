64687ac2b417baf5fec5cfd760fc3d2ee03e759d
mashen <undefined>
2022-10-30 22:20:14 +0800

    feat: 扑克牌中的顺子

---
+++ 数组/扑克牌中的顺子.js
@@ -0,0 +1,19 @@
+// 从若干副扑克牌中随机抽 5 张牌，判断是不是一个顺子，即这5张牌是不是连续的。
+// 2～10为数字本身，A为1，J为11，Q为12，K为13，而大、小王为 0 ，可以看成任意数字。A 不能视为 14。
+
+const isStraight = (nums) => {
+    nums.sort((a, b) => {
+        return a - b;
+    })
+    let zeroN = nums.lastIndexOf(0) + 1;
+    let restArr = nums.slice(nums.lastIndexOf(0) + 1);
+    let c = 0
+    for (let i = 0; i < restArr.length - 1; i++) {
+        if (restArr[i + 1] - restArr[i] > 1) {
+            c += restArr[i + 1] - restArr[i] - 1
+        } else if (restArr[i + 1] === restArr[i]) { // 只要有相等的元素，肯定不是顺子
+            return false;
+        }
+    }
+    return c <= zeroN;
+}
