87cada8c4ea2f4c33f1bc09fa62cc1fb69a77779
mashen <undefined>
2025-05-22 22:38:41 +0800

    feat: 柯里化

--- Js实现/函数柯里化.js
+++ Js实现/函数柯里化.js
@@ -2,15 +2,16 @@
  * 柯里化函数
  */
 // 木易杨
-function currying(fn, length) {
-    length = length || fn.length; // 注释 1
-    return function (...args) { // 注释 2
-        return args.length >= length // 注释 3
-            ?
-            fn.apply(this, args) // 注释 4
-            :
-            currying(fn.bind(this, ...args), ...args) // 注释 5
-    }
+function currying(func) {
+    return function curried(...args) {
+        if (args.length >= func.length) {
+            return func.apply(this, args);
+        } else {
+            return function (...args2) {
+                return curried.apply(this, args.concat(args2));
+            }
+        }
+    };
 }
 
 // Test
@@ -31,25 +32,25 @@ fn("a")("b", "c") // ["a", "b", "c"]
  */
 
 // 木易杨
-const add = (...args) => args.reduce((a, b) => a + b);
+// const add = (...args) => args.reduce((a, b) => a + b);
 
-// 简化写法
-function currying(func) {
-    let args = [];
-    return function result(...rest) {
-        if (rest.length === 0) {
-            const res = func(...args);
-            args = [];
-            return res;
-        } else {
-            args.push(...rest);
-            return result;
-        }
-    }
-}
+// // 简化写法
+// function currying(func) {
+//     let args = [];
+//     return function result(...rest) {
+//         if (rest.length === 0) {
+//             const res = func(...args);
+//             args = [];
+//             return res;
+//         } else {
+//             args.push(...rest);
+//             return result;
+//         }
+//     }
+// }
 
-const sum = currying(add);
+// const sum = currying(add);
 
-sum(1, 2)(3); // 未真正求值
-sum(4); // 未真正求值
-sum(); // 输出 10
+// sum(1, 2)(3); // 未真正求值
+// sum(4); // 未真正求值
+// sum(); // 输出 10
